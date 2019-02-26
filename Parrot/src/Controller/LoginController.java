973c715ee5c438a65d8f406ec199451cf34981be
tommee <tlg5058@gmail.com>
2019-02-25 22:54:45 -0500

    loginUI and loginController

--- Parrot/src/Controller/LoginController.java
+++ Parrot/src/Controller/LoginController.java
@@ -14,6 +14,8 @@ import Model.UserList;
 import Model.User;
 import Model.UserCredentials;
 import View.LoginScreen;
+// import View.LoginUI;
+// import javax.swing.JFrame;
 
 public class LoginController {
     
@@ -22,9 +24,23 @@ public class LoginController {
     private User activeUser;
     private UserCredentials activeCredentials;
     
+    // private LoginUI view;
+    // private JFrame frame;
+    // private String userName;
+    // private String password;
+    
     public LoginController() {
         
         loginScreen = new LoginScreen();
+        
+        // view = new LoginUI();
+        // Display
+        // frame = new JFrame();
+        // frame.add(view);
+        // frame.setVisible(true);
+        // frame.setSize(740, 520);
+        // frame.setLocationRelativeTo(null);
+        // frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         
     }
     
