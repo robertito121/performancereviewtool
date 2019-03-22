9f1494f78f3c603b55566d153e8312c252db9396
robertito121 <37167135+robertito121@users.noreply.github.com>
2019-03-21 23:49:30 -0400

    tying login view to home screen

---
+++ Parrot/src/View/HomeController.java
@@ -0,0 +1,43 @@
+package View;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+
+public class HomeController {
+
+    @FXML
+    private AnchorPane homeScreen;
+
+    @FXML
+    private Label myProfileFirstName;
+
+    @FXML
+    private Label myProfileLastName;
+
+    @FXML
+    private Label myProfileUserID;
+
+    @FXML
+    private Label myProfileRole;
+
+    @FXML
+    private Label userProfileFirstName;
+
+    @FXML
+    private Label userProfileLastName;
+
+    @FXML
+    private Label userProfileUserID;
+
+    @FXML
+    private Label userProfileRole;
+
+
+
+    public HomeController() {
+
+    }
+
+
+}
--- Parrot/src/View/LoginController.java
+++ Parrot/src/View/LoginController.java
@@ -2,16 +2,26 @@ package View;
 
 import Model.User;
 import Model.UserList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 
 public class LoginController {
 
+    @FXML
+    private AnchorPane loginPane;
     @FXML
     private javafx.scene.control.Label usernameLabel;
     @FXML
@@ -36,17 +46,27 @@ public class LoginController {
     }
 
     @FXML
-    public void authenticate() {
+    public void authenticate(ActionEvent event ) throws IOException {
 
         boolean isAuthenticated = checkAuthentication();
         if (isAuthenticated == true) {
-            System.out.println("authenticated");
+
+            //set homeView
+            Parent homeView = FXMLLoader.load(getClass().getResource("HomeView.fxml"));
+            Stage homeViewStage = new Stage();
+            homeViewStage.setTitle("Performance Review Tool (Parrot) ");
+            homeViewStage.setScene(new Scene(homeView));
+            homeViewStage.show();
+
+            //dispose login View
+            Stage loginStage = (Stage) loginButton.getScene().getWindow();
+            loginStage.close();
+
+
         }
         else {
             informationLabel.setTextFill(Color.RED);
             informationLabel.setText("The Username or Password combination is incorrect, please try again");
-
-
         }
      
     }
