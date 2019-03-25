28a31decab359b4d4050e33db6d14b5d6a573a96
robertito121 <37167135+robertito121@users.noreply.github.com>
2019-03-24 20:39:07 -0400

    Update HomeController.java

--- Parrot/src/View/HomeController.java
+++ Parrot/src/View/HomeController.java
@@ -8,6 +8,9 @@ import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
